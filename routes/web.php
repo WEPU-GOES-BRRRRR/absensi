de70e9d40a94cc4cedbf0b1b384e69d266c68141
RaflyNN27 <pinur271103@Gmail.com>
2024-06-14 23:23:57 +0700

    adding template from paper

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Frontcontroller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('landing', Frontcontroller::class);
